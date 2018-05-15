3621f5ea7fc1a77399f766330a5afb8bc74df598
Fabian Gerson <fabiangerson@googlemail.com>
2018-05-15 16:00:45 +0200

    initial

---
+++ src/SayHello.php
@@ -0,0 +1,9 @@
+    <?php 
+    namespace HelloWorld;
+    class SayHello
+    {
+        public static function goodbye()
+        {
+            return 'goodbye World, Composer!';
+        }
+    }
